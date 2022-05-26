12a28c3c67682dc97e2afc71c308867ae9a2e538
Данил <81508158+DanilSuhanov@users.noreply.github.com>
2022-05-26 23:21:16 +0300

    Добавьте файлы проекта.

---
+++ MyCity/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using MyCity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCity
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected  override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=MyCity;Username=postgres;Password=kadjitnichegonekral");
+        }
+    }
+}
